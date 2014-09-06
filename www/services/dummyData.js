65b1fe15eb6052cb343cf8ccf93b413b39e958d5
Bao <baoducng@gmail.com>
2014-09-06 11:30:22 -0700

    added business functionality for

--- www/services/dummyData.js
+++ www/services/dummyData.js
@@ -1,13 +1,13 @@
 angular.module('evernote').factory('dummyData', function(){
   var dummyData = {};
   dummyData.data = [
-    {front: 'What ashldhsjkfv',back: 'ok!!!!'},
-    {front: 'What a1shldhsjkfv',back: 'ok!11!!!'},
-    {front: 'What as2hldhsjkfv',back: '22ok!!!!'},
-    {front: 'What ash3ldhsjkfv',back: '333ok!!!!'},
-    {front: 'What ashl4dhsjkfv',back: '444ok!!!!'},
-    {front: 'What ashld5hsjkfv',back: '55ok!!!!'},
-    {front: 'What ashldh42576sjkfv',back: '66ok!!!!'}
+    {front: 'This is card 1',back: 'ok!!!!'},
+    {front: 'This is card 2',back: 'ok!11!!!'},
+    {front: 'This is card 3',back: '22ok!!!!'},
+    {front: 'This is card 4',back: '333ok!!!!'},
+    {front: 'This is card 5',back: '444ok!!!!'},
+    {front: 'This is card 6',back: '55ok!!!!'},
+    {front: 'This is card 7',back: '66ok!!!!'}
   ];
   return dummyData;
 });
--- www/tabs/review/review-controller.js
+++ www/tabs/review/review-controller.js
@@ -1,4 +1,24 @@
 angular.module('evernote.review.controller', [])
-.controller('reviewController', function($scope){
-  $scope.name = "LETS REVIEW SOME CARDS"
+.controller('reviewController', function($scope, dummyData){
+  $scope.current = 0;
+  $scope.currentCard = dummyData.data[$scope.current];
+  $scope.frontShowing = true;
+  //if there is no cards to review, show alert
+  $scope.message = 0;
+  if (dummyData.data.length === 0){
+    $scope.message = 1;
+  }
+  $scope.showBack = function(){
+    $scope.frontShowing = false;
+  }
+  $scope.next = function(){
+    $scope.current++;
+    $scope.currentCard = dummyData.data[$scope.current];
+    $scope.$apply();
+    $scope.frontShowing = true;
+    if ($scope.current === dummyData.data.length){
+      $scope.message = "Finished";
+    }
+  }
+
 })
