6756aa66f5e6a54f65037e7694d9f7959d0553b6
Bao <baoducng@gmail.com>
2014-09-06 13:05:53 -0700

    jenny

--- www/tabs/login/login-controller.js
+++ www/tabs/login/login-controller.js
@@ -1,7 +1,27 @@
 angular.module('evernote.login.controller', [])
-.controller('loginController', function($scope){
+.controller('loginController', function($scope, OAuth){
   $scope.cards = "OKAY THIS SHIT IS WORKING";
-  $scope.doLogin = function(){
-    console.log($scope.loginData);
-  }
+
+
+  // var hostName = "http://sandbox.evernote.com";
+  // var options,oauth;
+  // var options = {
+  //   consumerKey: 'baoducng',
+  //   consumerSecret: 'fc2054b3f5790757',
+  //   //what is this?
+  //   callbackUrl : hostName,
+  //   signatureMethod : "HMAC-SHA1",
+  // };
+  // oauth = OAuth(options);
+  // $scope.doLogin = function(){
+  //   oauth.request({
+  //     'method': 'GET',
+  //     'url': hostName + '/oauth',
+  //     'success': function(data){
+  //       $scope.cards = data;
+  //     },
+  //     'failure': function(error){
+  //       $scope.cards = error;
+  //     }});
+  // }
 })
