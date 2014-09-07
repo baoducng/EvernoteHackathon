210b31d13e2c52791c479995e0cc87cc20f156d7
Bao <baoducng@gmail.com>
2014-09-06 19:06:32 -0700

    123

--- www/tabs/allcards/allcards-controller.js
+++ www/tabs/allcards/allcards-controller.js
@@ -1,10 +1,15 @@
 angular.module('evernote.allcards.controller', [])
 .controller('allcardsController', function($scope, noteService){
-
+  $scope.selected = 0;
   noteService.getAll().then(function(data){
     $scope.cards = [].concat(data.due).concat(data.not_due);
   }, function(error){
     console.log(error)
   });
+  $scope.zoom = function(front, back){
+    $scope.selected = 1;
+    $scope.zoom1 = front;
+    $scope.zoom2 = back;
+  };
 
 })
